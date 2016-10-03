3d66572c8835727273f7009ce9278cd618b0d437
Harshil Naik <harshil.n@siliconbrain.in>
2016-10-03 15:05:36 +0530

    contact if already in database it can not insert.. retailsales clear the bug

--- retailInv.php
+++ retailInv.php
@@ -10,6 +10,21 @@ $inm = date_format($date,"Ymd");
 
 	if(isset($_POST['txtprdnm']))
 	{	
+		if(isset($_POST['txtmobno']))
+		{
+				
+			  $qry="select mobile_no from contact_dtl where mobile_no='".$_POST['txtmobno']."'";
+			  $res=mysql_query($qry);
+				if($res!='')
+			  {
+				
+				}
+			  else
+			 {
+				
+				$result=insertContactRetail($conn,$_POST['txtprdnm'],$_POST['txtmobno'],$_POST['txtadd']);
+			 }
+		  }
 		
 		// if($_POST['taxmode'] == 'Yes')
 		// {
@@ -82,21 +97,6 @@ $inm = date_format($date,"Ymd");
 		$_POST['txtcharge'],$_POST['txtpaid'],$_POST['txtdisc'],$nfrdt,$ntrdt,$cur_date,
 		$pay_status,$tax,$gtot,$_POST['txtstax'],$vat);
 		
-		if(isset($_POST['txtmobno']))
-		{
-				
-			  $qry="select mobile_no from contact_dtl where mobile_no='".$_POST['txtmobno']."'";
-			  $res=mysql_query($qry);
-				if($res!='')
-			  {
-				
-			}
-			  else
-			 {
-				
-				$result=insertContactRetail($conn,$_POST['txtprdnm'],$_POST['txtmobno'],$_POST['txtadd']);
-			 }
-		  }
 		
 		//select last record inserted from event_mst	
 		$eventlast_id = mysql_insert_id();;
